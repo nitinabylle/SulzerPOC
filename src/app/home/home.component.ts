aa8257f297c728ef06d5b8211d95ddfec981850b
nitinabylle <nitin.s@abylle.com>
2022-04-13 23:10:41 +0530

    design changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ViewChildren, QueryList, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { TreeModel } from 'src/app/model/tree.model';
+import { DropDownModel } from '../model/DropDownModel';
 
 // class FileFolders{
 //   constructor(
@@ -17,11 +18,19 @@ import { TreeModel } from 'src/app/model/tree.model';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements  OnInit {
-
+   trayType:any;
+   valueType:any;
+   fileToUpload: File | null = null;
   ngOnInit(): void {
-    
+    var dropDown= new DropDownModel();
+    this.trayType = dropDown['TrayType'];
+    this.valueType = dropDown['ValveType'];
   }
 
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+ }
+   
   // demoData:TreeModel;
   // demoData2:TreeModel;
   // treeData:FileFolders[];
---
+++ src/app/model/DropDownModel.ts
@@ -0,0 +1,32 @@
+export class DropDownModel {
+TrayType  = [
+    {id: 1, name: "Cartridge"},
+    {id: 2, name: "Conventional"},
+    {id: 3, name: "GT-EXTRACT"},
+    {id: 4, name: "Optim"},
+    {id: 5, name: "Plus Trays"},
+    {id: 6, name: "Shell"},
+    {id: 7, name: "Slit Tray"},
+    {id: 8, name: "Trays"}
+    ]
+
+   
+    PanelLayout  = [
+        {id: 1, name: "ip"},
+        {id: 2, name: "per"},
+        {id: 3, name: "ex"}
+     ]
+
+     ValveType   = [
+        {id: 1, name: "B3H"},
+        {id: 2, name: "B3X"},
+        {id: 3, name: "B3XC"},
+        {id: 4, name: "B4H"},
+        {id: 5, name: "B4X"},
+        {id: 6, name: "B4XC"},
+        {id: 7, name: "B6H"},
+        {id: 8, name: "B6X"}
+        ]
+
+    
+}
